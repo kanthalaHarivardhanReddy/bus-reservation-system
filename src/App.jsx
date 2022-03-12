b3aade2e935e9e05ecf086ca889647fc433b6897
Harivardhan <kanthala.hari.198@gmail.com>
2022-03-12 14:56:15 +0530

    booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Booking from "./Booking"
 const App = ()=>{
     return (
         <>
-            <h1>Hello, World!</h1>
+            <Booking />
         </>
     )
 }
